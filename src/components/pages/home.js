8f2eb2e39f54734f07c42f2843c4eb6392ed4a6d
smagucha <smagucha@gmail.com>
2021-02-24 23:11:42 +0300

    home

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -12,7 +12,7 @@ function Home() {
      <div className='divclass'>
        <img src={Code} />
        <div className='mainbox'>
-       <div className='box'>sammy magucha sammy magucha is full stack developer building web apps using django as the backend development
+       <div className='box'> sammy magucha is full stack developer building web apps using django as the backend development
        . Programming is passion as it can be difficult at times and challenging. The most interesting part about programming is that
        it offers one oppuntity to think and solve a problem uniquely</div>
        <div className='box'>
@@ -24,10 +24,14 @@ function Home() {
        <li>calclator with react</li>
        <li>ecommerce site</li>
        </div>
-       <div className='box'></div>
-       <div className='box'>projects</div>
+       <div className='box'>     “My job is challenging without being too stressful,
+        it gives me the time and resources to do what I want to do, 
+        and I work with really great people. For now, that’s pretty perfect.” </div>
+       <div className='box'>soft and technical articles</div>
        </div>
-       <div className='footer'> this is the footer</div>
+       <div className='footer'><p>name: sammy magucha</p><p>github: github.com/smagucha</p>
+       <p>linked: linkedin.com/in/smagucha</p><p>email: smagucha@gmail.com</p>
+       <p>phone: +254707181264</p></div>
     </div>
 
     </div>
